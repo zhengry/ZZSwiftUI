b9f684729670a0b8d411bc99e34b57b34e207966
zry <ruyanzheng@126.com>
2019-11-29 10:54:59 +0800

    给profile添加模糊背景

---
+++ ZZSwiftUI/BlurView.swift
@@ -0,0 +1,21 @@
+//
+//  BlurView.swift
+//  ZZSwiftUI
+//
+//  Created by zry on 2019/11/29.
+//  Copyright © 2019 ZRY. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
